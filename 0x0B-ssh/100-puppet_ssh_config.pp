0c1f9517edb2838a6f6eb1e312e28d78bda23dd5
IdaraKoffi <idarakoffi@gmail.com>
2023-11-26 11:29:41 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 # Changes SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
 }
